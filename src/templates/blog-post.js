d81fbcc6504e027c048239e7bdaceb1384d34140
mazaherm <mmuraj.313@gmail.com>
2018-10-11 12:16:59 +0100

    blog post path

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -16,3 +16,16 @@ export default function Template ({ data }) {
     </div>
   )
 }
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
